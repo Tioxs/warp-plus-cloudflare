a629bf11f7bbf7890b7f0b213bf81e3e2a337f54
xorcan <25418042+xorcan@users.noreply.github.com>
2020-05-31 14:44:01 +0300

    Update warp2.py

--- warp2.py
+++ warp2.py
@@ -1,4 +1,4 @@
-import requests
+import urllib.request
 import json
 import datetime
 import random
@@ -6,8 +6,8 @@ import string
 import time
 import os
 import sys
-os.system("title WARP-PLUS-CLOUDFLARE - XORCAN")
-os.system('cls')
+os.system("title WARP-PLUS-CLOUDFLARE - XORCAN v3")
+os.system('cls' if os.name == 'nt' else 'clear')
 print ("---------------------------------------------------------------------------")
 print ("[2] 1.1.1.1 için sınırsız WARP+ verisi metodu - sorusuz sürüm")
 print ("---------------------------------------------------------------------------")
@@ -38,7 +38,7 @@ def digitString(stringLength):
 		digit = string.digits
 		return ''.join((random.choice(digit) for i in range(stringLength)))    
 	except Exception as error:
-		print(error)    
+		print(error)	
 url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
 def run():
 	try:
@@ -51,15 +51,17 @@ def run():
 				"tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
 				"type": "Android",
 				"locale": "zh-CN"}
-		bodyString = json.dumps(body)
+		data = json.dumps(body).encode('utf8')
 		headers = {'Content-Type': 'application/json; charset=UTF-8',
 					'Host': 'api.cloudflareclient.com',
 					'Connection': 'Keep-Alive',
 					'Accept-Encoding': 'gzip',
 					'User-Agent': 'okhttp/3.12.1'
 					}
-		r = requests.post(url, data=bodyString, headers=headers)
-		return r
+		req         = urllib.request.Request(url, data, headers)
+		response    = urllib.request.urlopen(req)
+		status_code = response.getcode()	
+		return status_code
 	except Exception as error:
 		print(error)	
 
@@ -67,11 +69,11 @@ g = 0
 b = 0
 while True:
 	result = run()
-	if result.status_code == 200:
+	if result == 200:
 		g = g + 1
-		os.system('cls')
+		os.system('cls' if os.name == 'nt' else 'clear')
 		print("")
-		print("WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO / xorcan")
+		print("WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO / xorcan v3")
 		print("")
 		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
 		for i in range(len(animation)):
@@ -84,10 +86,10 @@ while True:
 		print("[*] 18 saniye sonra yeni istek gönderilecek.")
 		time.sleep(18)
 	else:
-		b = b + 1
-		os.system('cls')
+		os.system('cls' if os.name == 'nt' else 'clear')
 		print("")
-		print("WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO / xorcan")
+		print("WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO / xorcan v3")
 		print("")
 		print("[:(] Sunucuya bağlanırken hata oluştu")
-		print(f"[#] Toplam: {i} Başarılı {k} Başarısız")	
+		print(f"[#] Toplam: {i} Başarılı {k} Başarısız")
+		b = b + 1	
