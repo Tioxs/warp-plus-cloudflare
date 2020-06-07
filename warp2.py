6f20bcd4cbaadd24d5533eaf6831e671dacfb677
xorcan <25418042+xorcan@users.noreply.github.com>
2020-06-07 20:26:03 +0300

    Update warp2.py

--- warp2.py
+++ warp2.py
@@ -7,9 +7,10 @@ import time
 import os
 import sys
 import pathlib
-script_version = '3.0.0'
-window_title   = f"WARP-PLUS-CLOUDFLARE - ALIILAPRO / XORCAN (v {script_version})"
-os.system('title ' + window_title if os.name == 'nt' else 'PS1="\[\e]0;' + window_title + '\a\]"; echo $PS1')
+
+script_version = '4.0.0'
+script_title   = f"WARP-PLUS-CLOUDFLARE - ALIILAPRO / XORCAN (v {script_version})"
+os.system('title ' + script_title if os.name == 'nt' else 'PS1="\[\e]0;' + script_title + '\a\]"; echo $PS1')
 os.system('cls' if os.name == 'nt' else 'clear')
 print ("---------------------------------------------------------------------------")
 print ("[2] 1.1.1.1 için sınırsız WARP+ verisi (betik) - sorusuz sürüm")
@@ -40,13 +41,16 @@ def progressBar():
 	while True:
 		for i in range(10):
 			percent += 1
-			sys.stdout.write(f"\r[+] Yanıt bekleniyor...      " + save_anim + f" %{percent}")
+			sys.stdout.write(f"\r[+] İstek gönderiliyor... " + save_anim + f" %{percent}")
 			sys.stdout.flush()
-			time.sleep(0.075)
+			if result.is_alive():
+				time.sleep(0.1)
+			else:
+				time.sleep(0.005)
 		progress_anim += 1
 		save_anim = animation[progress_anim % len(animation)]
 		if percent == 100:
-			sys.stdout.write("\r[+] İstek tamamlandı...      [■■■■■■■■■■] %100")
+			sys.stdout.write("\r[+] Yanıt alındı.         [■■■■■■■■■■] %100")
 			break
 
 def genString(stringLength):
@@ -54,65 +58,70 @@ def genString(stringLength):
 		letters = string.ascii_letters + string.digits
 		return ''.join(random.choice(letters) for i in range(stringLength))
 	except Exception as error:
-		print(error)		    
+		print(error)	
+
 def digitString(stringLength):
 	try:
 		digit = string.digits
 		return ''.join((random.choice(digit) for i in range(stringLength)))    
 	except Exception as error:
-		print(error)	
+		print(error)
+
 url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
-def run():
-	try:
-		install_id = genString(22)
-		body = {"key": "{}=".format(genString(43)),
-				"install_id": install_id,
-				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-				"referrer": referrer,
-				"warp_enabled": False,
-				"tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
-				"type": "Android",
-				"locale": "zh-CN"}
-		data = json.dumps(body).encode('utf8')
-		headers = {'Content-Type': 'application/json; charset=UTF-8',
+
+from threading import Thread
+
+class sendRequest(Thread):
+	def run(self):
+		try:
+			install_id  = genString(22)
+			body        = {"key": "{}=".format(genString(43)),
+					"install_id": install_id,
+					"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+					"referrer": referrer,
+					"warp_enabled": False,
+					"tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
+					"type": "Android",
+					"locale": "zh-CN"}
+			data        = json.dumps(body).encode('utf8')
+			headers     = {'Content-Type': 'application/json; charset=UTF-8',
 					'Host': 'api.cloudflareclient.com',
 					'Connection': 'Keep-Alive',
 					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'
-					}
-		req         = urllib.request.Request(url, data, headers)
-		response    = urllib.request.urlopen(req)
-		status_code = response.getcode()	
-		return status_code
-	except Exception as error:
-		print("")
-		print(error)	
+					'User-Agent': 'okhttp/3.12.1'}
+			req         = urllib.request.Request(url, data, headers)
+			response    = urllib.request.urlopen(req)
+			status_code = response.getcode()
+			self.status = status_code
+		except Exception as error:
+			self.status = error
 
 g = 0
 b = 0
 while True:
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print("")
-	print("WARP-PLUS-CLOUDFLARE (betik)" + " - ALIILAPRO / XORCAN v3")
+	print(f"{script_title}")
 	print("")
-	sys.stdout.write("\r[+] İstek gönderiliyor...    [□□□□□□□□□□] %0")
-	sys.stdout.flush()
-	result = run()
-	if result == 200:
+	result = sendRequest()
+	result.start()
+	print(f"[-] Üzerinde çalışılan kimlik: {referrer}")
+	progressBar()
+	if result.status == 200:
 		g += 1
-		progressBar()
-		print(f"\n[-] Şu kimlik üzerinde çalışılıyor: {referrer}")    
+		print(f"\n[#] Toplam: {g} Başarılı {b} Başarısız.")
+		print("")
 		print(f"[:)] {g} GB kullanım hakkı hesabınıza eklendi.")
-		print(f"[#] Toplam: {g} Başarılı, {b} Başarısız.")
 		for i in range(18,0,-1):
 			sys.stdout.write(f"\r[*] {i} saniye sonra yeni bir istek gönderilecek.")
 			sys.stdout.flush()
 			time.sleep(1)
 	else:
 		b += 1
-		print("[:(] Sunucuya bağlanırken hata oluştu.")
-		print(f"[#] Toplam: {g} Başarılı, {b} Başarısız.")
-		for i in range(10,0,-1):
+		print(f"\n[#] Toplam: {g} Başarılı {b} Başarısız.")
+		print("")
+		print("[:(] Sunucuya bağlanırken hata oluştu: " + str(result.status))
+		for i in range(18,0,-1):
 			sys.stdout.write(f"\r[*] {i} saniye içinde tekrar deneniyor...")
 			sys.stdout.flush()
 			time.sleep(1)
