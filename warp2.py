9bc52d0290629251a5e7902cc8f33888009eabba
xorcan <25418042+xorcan@users.noreply.github.com>
2020-06-08 15:39:02 +0300

    Update warp2.py

--- warp2.py
+++ warp2.py
@@ -107,6 +107,7 @@ while True:
 	result.start()
 	print(f"[-] Üzerinde çalışılan kimlik: {referrer}")
 	print(f"[#] Toplam: {g} Başarılı, {b} Başarısız.")
+	print("")
 	progressBar()
 	if result.status == 200:
 		g += 1
